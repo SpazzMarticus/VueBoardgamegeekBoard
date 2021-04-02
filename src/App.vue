91b4303d05d5d54bd6356f3b37ddb2636e2ce955
SpazzMarticus <SpazzMarticus@users.noreply.github.com>
2021-04-02 19:19:12 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,208 @@
+<template>
+  <div v-if="showForm">
+    <label>
+      BoardGameGeek - Username<br />
+      <input v-model="username" v-on:keyup.enter="submit" />
+      <small>
+        Enter your BGG username and submit.
+        <br />
+      </small>
+    </label>
+    <label>
+      <button type="button" @click="submit" :disabled="!username">
+        Submit
+      </button>
+    </label>
+  </div>
+  <div v-else class="flex">
+    <a class="button" v-if="permalink" :href="permalink">Permalink</a>
+    <button type="button" @click="reset">Reset</button>
+  </div>
+  <Board :username="submittedUsername" v-if="submittedUsername" />
+</template>
+
+<script>
+import Board from "./components/Board.vue";
+
+let username = null;
+
+/**
+ * Fetch username from query string, if possible
+ */
+const params = new URLSearchParams(window.location.search);
+if (params.has("username")) {
+  username = params.get("username");
+}
+
+const pushUri = (username = null) => {
+  let url = new URL(window.location);
+  let key = "username";
+  if (username) {
+    url.searchParams.set(key, username);
+  } else {
+    url.searchParams.delete(key);
+  }
+  window.history.pushState({}, "", url);
+};
+
+export default {
+  name: "App",
+  components: {
+    Board,
+  },
+  data() {
+    return {
+      username,
+      showForm: !username,
+      submittedUsername: username ?? null,
+    };
+  },
+  computed: {
+    permalink() {
+      if (this.username) {
+        return `https://spazzmarticus.github.io/VueBoardgamegeekBoard/?username=${this.username}`;
+      }
+      return null;
+    },
+  },
+  methods: {
+    reset() {
+      this.showForm = true;
+      this.submittedUsername = null;
+      pushUri();
+    },
+    submit() {
+      this.showForm = false;
+      this.submittedUsername = this.username;
+      pushUri(this.username);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "node_modules/normalize.css/normalize";
+
+$bodyPadding: 1rem;
+$playAreaPadding: 0.5rem;
+$playAreaSelectedMargin: 0.5rem;
+$defaultBorderRadius: 0.33rem;
+$diceSize: 3.5rem;
+$gridGap: 1rem;
+
+$themeBackground: #fdfdfd;
+$themeLinkColor: rgb(31, 124, 82);
+$themeTextLight: #575757;
+$themeSelected: #3b3a42;
+$themeTable: #d1b494;
+$themeTray: #cccccc;
+$themeSettings: #dedede;
+
+$minWidthSmall: (2 * $bodyPadding)+ (4 * $playAreaPadding)+
+  (2 * $playAreaSelectedMargin)+ (6 * $gridGap)+ (7 * $diceSize);
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 1rem;
+  font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue",
+    Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+    "Segoe UI Symbol", "Noto Color Emoji";
+  background-color: $themeBackground;
+  height: 100vh;
+}
+
+body {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 5px $bodyPadding;
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow-y: scroll;
+}
+
+a {
+  color: $themeLinkColor;
+  text-decoration: none;
+  text-decoration: underline;
+}
+
+.header {
+  color: $themeTextLight;
+  text-align: center;
+
+  h1 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0 0 1rem 0;
+  }
+}
+
+.footer {
+  color: $themeTextLight;
+  padding: 1rem;
+  font-size: 0.7rem;
+  text-align: center;
+  line-height: 1.5;
+  margin-top: auto;
+}
+
+label {
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  line-height: 1.5;
+  margin-bottom: 1rem;
+}
+input {
+  display: block;
+  border: 1px solid grey;
+  border-radius: 4px;
+  padding: 4px;
+  margin: 4px 0;
+  line-height: 1;
+}
+button,
+.button {
+  display: inline-block;
+  font-size: 100%;
+  padding: 4px;
+  border-radius: 4px;
+  background-color: $themeLinkColor;
+  box-shadow: none;
+  color: white;
+  border: 2px solid $themeLinkColor;
+  cursor: pointer;
+  text-decoration: none;
+  line-height: 1;
+  &[disabled] {
+    background-color: #3b3a42;
+    border-color: #3b3a42;
+    cursor: not-allowed;
+  }
+
+  .icon{
+    height: 1.5rem;
+    widows: 1.5rem;
+  }
+}
+
+.flex {
+  display: flex;
+  justify-content: space-between;
+}
+.center{
+  display: flex;
+  justify-content: center;
+}
+
+pre {
+  border: 1px solid black;
+}
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,173 @@
+<template>
+  <div>
+    <div v-if="!loaded" class="loading">
+      Loading board for <i>{{ username }}</i
+      >...
+    </div>
+    <div class="board" v-if="loaded">
+      <div v-if="collection.length > 0">
+        <div v-if="randomLeastPlayed" class="random">
+          <Game class="game" :game="randomLeastPlayed">
+            <template #button>
+              <button @click="reroll">
+                <img class="icon" src="@/assets/cycle.svg" />
+              </button>
+            </template>
+          </Game>
+        </div>
+
+        <div class="center">
+          <select v-model="sortBy">
+            <option
+              v-for="(sortOption, key) in sortOptions"
+              :key="key"
+              :value="sortOption"
+            >
+              {{ sortOption.name }}
+            </option>
+          </select>
+        </div>
+
+        <div class="shelf">
+          <Game
+            class="game"
+            v-for="game in collectionSorted"
+            :key="game.id"
+            :game="game"
+          ></Game>
+        </div>
+      </div>
+      <div v-else>
+        <h1>Whoops!</h1>
+        <p>You don't have any games in your collection.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Request from "../modules/request.js";
+import Game from "./Game.vue";
+
+const sortOptions = [
+  {
+    name: "latest plays",
+    sort(a, b) {
+      if (a.lastPlayAgo === null && b.lastPlayAgo === null) {
+        return 0;
+      } else if (a.lastPlayAgo === null) {
+        return 1;
+      } else if (b.lastPlayAgo === null) {
+        return -1;
+      }
+      return a.lastPlayAgo <= b.lastPlayAgo ? -1 : 1;
+    },
+  },
+  {
+    name: "most plays",
+    sort(a, b) {
+      if (a.playCount === null && b.playCount === null) {
+        return 0;
+      } else if (a.playCount === null) {
+        return 1;
+      } else if (b.playCount === null) {
+        return -1;
+      }
+      return a.playCount >= b.playCount ? -1 : 1;
+    },
+  },
+];
+
+export default {
+  name: "Board",
+  components: {
+    Game,
+  },
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      loaded: false,
+      collection: null,
+      sortOptions,
+      sortBy: sortOptions[0],
+      randomLeastPlayed: null,
+    };
+  },
+  mounted() {
+    Request.fetch(this.username).then((collection) => {
+      this.collection = collection;
+      this.reroll();
+      this.loaded = true;
+    });
+  },
+  methods: {
+    pickLeastPlayed() {
+      let min = 0;
+      this.collection.reduce((min, game) => {
+        if (game.playCount < min) {
+          min = game.playCount;
+        }
+      }, min);
+
+      let leastPlayed = this.collection.filter((game) => {
+        if (game.playCount === min) {
+          return true;
+        }
+        return false;
+      });
+
+      this.randomLeastPlayed =
+        leastPlayed[Math.floor(Math.random() * leastPlayed.length)];
+    },
+    reroll() {
+      this.pickLeastPlayed();
+    },
+  },
+  computed: {
+    collectionSorted() {
+      return this.collection.slice().sort(this.sortBy.sort);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.board {
+  display: flex;
+  flex-direction: column;
+}
+
+.loading {
+  padding: 2rem;
+  font-size: 120%;
+}
+
+.random {
+  margin-bottom: 1rem;
+}
+
+.shelf {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 4px;
+  @media screen and (min-width: 600px) {
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+  }
+
+  .game {
+    margin-top: 0;
+  }
+}
+
+.game {
+  margin-top: 1rem;
+  padding: 6px;
+}
+</style>
